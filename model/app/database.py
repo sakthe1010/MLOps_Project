9436c74a46b9ed14d28cbb0a73c685176d2fd20d
retro-31 <me21b014@smail.iitm.ac.in>
2025-04-29 15:05:53 +0530

    user auth using postgres

--- model/app/database.py
+++ model/app/database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite database URL
-DATABASE_URL = "sqlite:///./users.db"
+# OLD (SQLite):
+# DATABASE_URL = "sqlite:///./users.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# NEW (Postgres):
+DATABASE_URL = "postgresql://mcq_user:mcq_pass@localhost/mcq_db"
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
